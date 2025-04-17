b47aa67299df4d5d0b5c47c2840a9e57847fbe89
Vivek <vivekdesai1215@gmail.com>
2025-04-18 01:05:41 +0530

    Creating an Express Server

---
+++ devAdda-backend/src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server is up and running...");
+});
+
+app.use((req, res) => {
+  res.send("Hello from the server 👋");
+});
+
+app.use("/test",(req,res)=>{
+    res.send("HEllo this is testing page !");
+})
+
