5928a519b51001c02c8e20bd0117937ddb6a0e71
Vivek <vivekdesai1215@gmail.com>
2025-05-20 14:16:20 +0530

    bug fixes

--- devAdda-backend/src/config/database.js
+++ devAdda-backend/src/config/database.js
@@ -2,8 +2,13 @@ const mongoose = require("mongoose");
 require("dotenv").config({ path: "./secrets.env" });
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.DB_CONNECTION_STRING);
-  console.log("Mongo URI:", process.env.DB_CONNECTION_STRING);
+  try {
+    await mongoose.connect(process.env.DB_CONNECTION_STRING);
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.error("Error connecting to database:", error.message);
+    throw error; // important to throw so app knows connection failed
+  }
 };
 
 module.exports = connectDB;
