3f1b3bae97e874fae6b8209af7b7397105abf3e6
Vivek <vivekdesai1215@gmail.com>
2025-05-20 14:40:29 +0530

    fixing backend DB

--- devAdda-backend/src/app.js
+++ devAdda-backend/src/app.js
@@ -1,13 +1,15 @@
 const express = require("express");
+const path = require("path");
 const connectDB = require("./config/database");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const app = express();
 
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
-const cors = require("cors");
+
 connectDB()
   .then(() => {
     console.log("database connection established");
@@ -17,7 +19,7 @@ connectDB()
   })
   .catch((err) => {
     console.error("Database cannot be connected:", err.message);
-    process.exit(1); // exit with error code so PM2 knows something failed
+    process.exit(1);
   });
 
 app.use(
@@ -26,6 +28,7 @@ app.use(
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -34,60 +37,10 @@ app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
 
-// app.get("/user", async (req, res) => {
-//   const userEmail = req.body.emailId;
-//   try {
-//     const users = await User.find({ emailId: userEmail });
-//     if (users.length === 0) res.status(404).send("user not found");
-//     else res.send(users);
-//   } catch (err) {
-//     res.status(500).send("Error !!");
-//   }
-// });
+// Serve React build static files
+app.use(express.static(path.join(__dirname, "../client/build")));
 
-// app.get("/feed", async (req, res) => {
-//   try {
-//     const users = await User.find({});
-//     if (users.length === 0) res.send("DataBase is Empty");
-//     else res.send(users);
-//   } catch (err) {
-//     res.status(500).send("Something went wrong");
-//   }
-// });
-
-// app.delete("/user", async (req, res) => {
-//   const userId = req.body.userId;
-//   try {
-//     const user = await User.findByIdAndDelete(userId);
-//     //can also use User.findByIdAndDelete({_id : userId});
-//     res.send("User Deleted Successfully");
-//   } catch (err) {
-//     res.status(500).send("Something went wrong");
-//   }
-// });
-
-// app.patch("/user", async (req, res) => {
-//   const userId = req.body.userId;
-//   const data = req.body;
-//   try {
-//     const ALLOWED_UPDATES = ["photoURL", "about", "gender", "age", "skills"];
-
-//     const isUpdateAllowed = Object.keys(data).every((k) =>
-//       ALLOWED_UPDATES.includes(k)
-//     );
-//     if (!isUpdateAllowed) {
-//       throw new Error("Unupdtable fields");
-//     }
-//     const userSkills = data.skills;
-//     if (data.skills.length > 15) {
-//       throw new Error("Bro don't lie ! U can't have skills more than 15");
-//     }
-
-//     const user = await User.findByIdAndUpdate(userId, data, {
-//       runValidators: true,
-//     });
-//     res.send(`User Updated Successfully`);
-//   } catch (err) {
-//     res.status(500).send("Something went wrong");
-//   }
-// });
+// For any other route, serve React app's index.html (handles frontend routing)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
